af19b652897d57e409d4a59480726a4d16ba4d9e
Shriram Mange <shrirammange12345@gmail.com>
2025-03-24 20:22:57 +0530

    la la lalalallaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+// Only import web plugins when running on web
+// Remove direct import of flutter_web_plugins
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'styles/theme.dart';
@@ -11,14 +12,18 @@ import 'models/course.dart';
 import 'models/timetable_schedule.dart';
 import 'utils/storage_helper.dart';
 
+// Use conditional import for web platform
+// We'll import this only when compiling for web
+import 'web_url_strategy.dart' if (dart.library.io) 'stub_url_strategy.dart';
+
 void main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
     
     // Configure web specific settings
     if (kIsWeb) {
-      usePathUrlStrategy();
-      setUrlStrategy(PathUrlStrategy());
+      // Call the platform-independent function
+      configureUrl();
     }
     
     runApp(const MyApp());
